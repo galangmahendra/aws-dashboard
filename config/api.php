62d0b1a9d1cae4f5a85146bdbb51ff84ed2fb96a
galangmahendra <61130569+galangmahendra@users.noreply.github.com>
2020-05-15 15:38:33 +0700

    Update values

--- config/api.php
+++ config/api.php
@@ -24,14 +24,7 @@ while ($row = mysqli_fetch_array($sql)) {
     $data[$row['time']] = $row['val'];
 }
 
-// generate data
-$temp = 32; // start sequence value
-$temp_interval = [-0.3,-0.2,-0.1,0,0.1];
-$noise = 44; // start sequence value
-$noise_interval = [-1,0,1];
-
 $dataview_total = 15;
-//$time = date("Y-m-d H:i");
 $time = date("Y-m-d H:i", time() - 450);
 for( $i = 1; $i <= $dataview_total; $i++ ) {
     $time = date("Y-m-d H:i:s", strtotime($time) + 30);
@@ -52,20 +45,18 @@ for( $i = 1; $i <= $dataview_total; $i++ ) {
         );
         $dataview[] = array_merge(array('time'=>$time),$values);
     } else {
-        $temp = sequence($temp,$temp_interval);
-        $noise = sequence($noise,$noise_interval);
         $values = array(
-            'pm2' => rand(4,27),
-            'pm10' => rand(4,28),
-            'so2' => rand(8,22),
-            'no2' => rand(19,34),
-            'co' => '0.'.rand(730,975),
-            'o3' => rand(19,31),
-            'ispu' => rand(19,31),
+            'pm2' => '7.'.rand(0,30),
+            'pm10' => '17.'.rand(0,50),
+            'so2' => rand(20,23).'.'.rand(10,99),
+            'no2' => rand(9,11).'.'.rand(10,99),
+            'co' => rand(248,249).'.'.rand(100,999),
+            'o3' => rand(70,72),
+            'ispu' => '10',
             'rem' => 'Baik',
-            't' => $temp,
-            'rh' => rand(43,62).'.'.rand(1,10),
-            'hc' => $noise,
+            't' => '30.'.rand(1,7),
+            'rh' => rand(60,62),
+            'hc' => '0',
         );
         $dataview[] = array_merge(array('time'=>$time),$values);
         $values_all = implode('|',$values);
